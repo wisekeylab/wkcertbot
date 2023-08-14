5474421c54d4bd8f00ca59fa4dc3eb1a052317b4
Pedro F <34808198+pfuentes69@users.noreply.github.com>
2023-08-14 14:11:31 +0200

    Update get-wkcertbot.sh

--- get-zerosslbot.sh
+++ get-zerosslbot.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 
-ZEROSSLBOT_SCRIPT_LOCATION=${ZEROSSLBOT_SCRIPT_LOCATION-"https://raw.githubusercontent.com/zerossl/zerossl-bot/master/zerossl-bot.sh"}
+WKCERTBOT_SCRIPT_LOCATION=${WKCERTBOT_SCRIPT_LOCATION-"https://raw.githubusercontent.com/wisekeylab/wkcertbot/master/get-wkcertbot.sh"}
 
-function install_zerosslbot()
+function install_wkcertbot()
 {
-    curl -s "$ZEROSSLBOT_SCRIPT_LOCATION" > /tmp/zerossl-bot
+    curl -s "$WKCERTBOT_SCRIPT_LOCATION" > /tmp/wkcertbot
     sudo bash <<EOF
         mkdir -p /usr/local/bin && \
-        mv /tmp/zerossl-bot /usr/local/bin/zerossl-bot && \
-        chmod +x /usr/local/bin/zerossl-bot
+        mv /tmp/wkcertbot /usr/local/bin/wkcertbot && \
+        chmod +x /usr/local/bin/wkcertbot
 EOF
 }
 
-install_zerosslbot
+install_wkcertbot
